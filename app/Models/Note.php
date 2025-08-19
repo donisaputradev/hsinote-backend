39ffb22f127a55d717f34564e527b89d2a6cfca3
donisaputradev <donisaputradev@gamail.com>
2025-08-19 10:37:37 +0700

    first commit

---
+++ app/Models/Note.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    use HasUlids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $guarded = ['id'];
+}
